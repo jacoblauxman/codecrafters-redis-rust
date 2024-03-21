617204c98c44ab903fe3736a1b3a56e18843083a
Jacob Lauxman <jlauxman@gmail.com>
2024-03-20 19:12:53 -0700

    4th stage attempt

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,62 @@
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+use anyhow::Context;
+use bytes::{BufMut, BytesMut};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379")
+        .await
+        .context("Failed to bind TcpListener to address")?;
+
+    loop {
+        match listener.accept().await {
+            Ok((mut stream, _socketaddr)) => {
                 println!("accepted new connection");
-                loop {
-                    connection_handler(&mut stream);
-                }
+
+                tokio::spawn(async move {
+                    if let Err(err) = connection_handler(&mut stream).await {
+                        eprintln!("Error handling connection: {:?}", err);
+                    }
+                });
             }
-            Err(e) => {
-                println!("error: {}", e);
+            Err(err) => {
+                println!("ERROR: {}", err);
             }
-        }
+        };
     }
 }
 
-fn connection_handler(stream: &mut TcpStream) {
-    let mut req_buf = [0; 128];
-    let _req_read = stream.read(&mut req_buf);
+async fn connection_handler(stream: &mut TcpStream) -> anyhow::Result<()> {
+    let mut res_buf = BytesMut::new();
+    let mut req_buf = BytesMut::new();
 
-    stream
-        .write("+PONG\r\n".as_bytes())
-        .expect("Faield to write PONG response to stream");
+    loop {
+        req_buf.reserve(512);
+        let req_read = stream
+            .read_buf(&mut req_buf)
+            .await
+            .context("Failed to read in request from TcpStream")?;
+
+        if req_read == 0 {
+            break;
+        }
+
+        res_buf.put_slice(b"+PONG\r\n");
+        stream
+            .write_all(&res_buf)
+            .await
+            .context("Failed to write response buffer to TcpStream")?;
+
+        stream
+            .flush()
+            .await
+            .context("Failed to flush TcpStream response writer")?;
+        res_buf.clear();
+        req_buf.clear();
+    }
+
+    Ok(())
 }
